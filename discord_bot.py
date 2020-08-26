cb3d2693dd310d355dae1ec05c23c9fbb1b2708e
alphascriptyt <oliver.pickford@outlook.com>
2020-08-26 12:29:59 +0100

    Episode 6 Logging\Loading

--- discord_bot.py
+++ discord_bot.py
@@ -1,89 +1,122 @@
 import discord
+import aiofiles
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix = "£")
-
-class BotData:
-    def __init__(self):
-        self.welcome_channel = None
-        self.goodbye_channel = None
-
-botdata = BotData()
 
 #bot events
 @bot.event
 async def on_ready():
     print("Your bot is ready.")
     bot.reaction_roles = []
+    bot.welcome_channels = {} # store like {guild_id : (channel_id, message)}
+    bot.goodbye_channels = {}
+    
+    for file in ["reaction_roles.txt", "welcome_channels.txt", "goodbye_channels.txt"]:
+        async with aiofiles.open(file, mode="a") as temp:
+            pass
+
+    async with aiofiles.open("reaction_roles.txt", mode="r") as file:
+        lines = await file.readlines()
+        for line in lines:
+            data = line.split(" ")
+            bot.reaction_roles.append((int(data[0]), int(data[1]), data[2].strip("\n")))
+
+    async with aiofiles.open("welcome_channels.txt", mode="r") as file:
+        lines = await file.readlines()
+        for line in lines:
+            data = line.split(" ")
+            bot.welcome_channels[int(data[0])] = (int(data[1]), " ".join(data[2:]).strip("\n"))
+
+    async with aiofiles.open("goodbye_channels.txt", mode="r") as file:
+        lines = await file.readlines()
+        for line in lines:
+            data = line.split(" ")
+            bot.goodbye_channels[int(data[0])] = (int(data[1]), " ".join(data[2:]).strip("\n"))
 
 @bot.event
 async def on_raw_reaction_add(payload):
-    for role, msg, emoji in bot.reaction_roles:
-        if msg.id == payload.message_id and emoji == payload.emoji.name:
-            await payload.member.add_roles(role)
+    for role_id, msg_id, emoji in bot.reaction_roles:
+        if msg_id == payload.message_id and emoji == str(payload.emoji.name.encode("utf-8")):
+            guild = bot.get_guild(payload.guild_id)
+            await guild.get_member(payload.user_id).add_roles(guild.get_role(role_id))
 
 @bot.event
 async def on_raw_reaction_remove(payload):
-    for role, msg, emoji in bot.reaction_roles:
-        if msg.id == payload.message_id and emoji == payload.emoji:
-            await bot.get_guild(payload.guild_id).get_member(payload.user_id).remove_roles(role)
+    for role_id, msg_id, emoji in bot.reaction_roles:
+        if msg_id == payload.message_id and emoji == str(payload.emoji.name.encode("utf-8")):
+            guild = bot.get_guild(payload.guild_id)
+            await guild.get_member(payload.user_id).remove_roles(guild.get_role(role_id))
 
 @bot.event
 async def on_member_join(member):
-    if botdata.welcome_channel != None:
-        await botdata.welcome_channel.send(f"Welcome! {member.mention}")
-
-    else:
-        print("Welcome channel was not set.")
+    for guild_id in bot.welcome_channels:
+        if guild_id == member.guild.id:
+            channel_id, message = bot.welcome_channels[guild_id]
+            await bot.get_guild(guild_id).get_channel(channel_id).send(f"{message} {member.mention}")
+            return
 
 @bot.event
 async def on_member_remove(member):
-    if botdata.goodbye_channel != None:
-        await botdata.goodbye_channel.send(f"Goodbye! {member.mention}")
-
-    else:
-        print("Goodbye channel was not set.")
+    for guild_id in bot.goodbye_channels:
+        if guild_id == member.guild.id:
+            channel_id, message = bot.goodbye_channels[guild_id]
+            await bot.get_guild(guild_id).get_channel(channel_id).send(f"{message} {member.mention}")
+            return
 
 #bot commands
 @bot.command()
-async def set_reaction(ctx, role: discord.Role, msg: discord.Message, emoji):
-    # Optional for custom emojis, not tested.
-    if emoji.isdigit():
-        try:
-            emoji = bot.get_emoji(emoji).name
-        except:
-            emoji = None
-    
+async def set_reaction(ctx, role: discord.Role=None, msg: discord.Message=None, emoji=None):
     if role != None and msg != None and emoji != None:
         await msg.add_reaction(emoji)
-        bot.reaction_roles.append((role, msg, emoji))
+        bot.reaction_roles.append((role, msg, str(emoji.encode("utf-8"))))
+        
+        async with aiofiles.open("reaction_roles.txt", mode="a") as file:
+            emoji_utf = emoji.encode("utf-8")
+            await file.write(f"{role.id} {msg.id} {emoji_utf}\n")
+
+        await ctx.channel.send("Reaction has been set.")
         
     else:
         await ctx.send("Invalid arguments.")
 
 @bot.command()
-async def set_welcome_channel(ctx, channel_name=None):
-    if channel_name != None:
+async def set_welcome_channel(ctx, new_channel: discord.TextChannel=None, *, message=None):
+    if new_channel != None and message != None:
         for channel in ctx.guild.channels:
-            if channel.name == channel_name:
-                botdata.welcome_channel = channel
-                await ctx.channel.send(f"Welcome channel has been set to: {channel.name}")
+            if channel == new_channel:
+                bot.welcome_channels[ctx.guild.id] = (channel.id, message)
+                await ctx.channel.send(f"Welcome channel has been set to: {channel.name} with the message {message}")
                 await channel.send("This is the new welcome channel!")
+                
+                async with aiofiles.open("welcome_channels.txt", mode="a") as file:
+                    await file.write(f"{ctx.guild.id} {new_channel.id} {message}\n")
+
+                return
+
+        await ctx.channel.send("Couldn't find the given channel.")
 
     else:
-        await ctx.channel.send("You didn't include the name of a welcome channel.")
+        await ctx.channel.send("You didn't include the name of a welcome channel or a welcome message.")
 
 @bot.command()
-async def set_goodbye_channel(ctx, channel_name=None):
-    if channel_name != None:
+async def set_goodbye_channel(ctx, new_channel: discord.TextChannel=None, *, message=None):
+    if new_channel != None and message != None:
         for channel in ctx.guild.channels:
-            if channel.name == channel_name:
-                botdata.goodbye_channel = channel
-                await ctx.channel.send(f"Goodbye channel has been set to: {channel.name}")
+            if channel == new_channel:
+                bot.goodbye_channels[ctx.guild.id] = (channel.id, message)
+                await ctx.channel.send(f"Goodbye channel has been set to: {channel.name} with the message {message}")
                 await channel.send("This is the new goodbye channel!")
+                
+                async with aiofiles.open("goodbye_channels.txt", mode="a") as file:
+                    await file.write(f"{ctx.guild.id} {new_channel.id} {message}\n")
+
+                return
+
+        await ctx.channel.send("Couldn't find the given channel.")
 
     else:
-        await ctx.channel.send("You didn't include the name of a goodbye channel.")
+        await ctx.channel.send("You didn't include the name of a goodbye channel or a goodbye message.")
 
 @bot.command()
 async def hello(ctx):

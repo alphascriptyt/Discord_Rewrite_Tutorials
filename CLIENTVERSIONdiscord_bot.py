147e91b4ec6e48f4c9534914f102e095a2db7b11
Spaxly <68564017+Spaxly@users.noreply.github.com>
2021-01-05 18:13:06 -0500

    Create CLIENTVERSIONdiscord_bot.py
    Here is the "client" version of the code. This is for people who use "client" instead of "bot".

---
+++ CLIENTVERSIONdiscord_bot.py
@@ -0,0 +1,270 @@
+# THIS IS FOR PEOPLE WHO USE CLIENT INSTEAD OF BOT
+
+@client.event
+async def on_ready():
+    client.reaction_roles = []
+    client.welcome_channels = {} # store like {guild_id : (channel_id, message)}
+    client.goodbye_channels = {}
+    client.sniped_messages = {}
+    client.ticket_configs = {}
+    
+    for file in ["reaction_roles.txt", "welcome_channels.txt", "goodbye_channels.txt", "ticket_configs.txt"]:
+        async with aiofiles.open(file, mode="a") as temp:
+            pass
+
+    async with aiofiles.open("reaction_roles.txt", mode="r") as file:
+        lines = await file.readlines()
+        for line in lines:
+            data = line.split(" ")
+            client.reaction_roles.append((int(data[0]), int(data[1]), data[2].strip("\n")))
+
+    async with aiofiles.open("welcome_channels.txt", mode="r") as file:
+        lines = await file.readlines()
+        for line in lines:
+            data = line.split(" ")
+            client.welcome_channels[int(data[0])] = (int(data[1]), " ".join(data[2:]).strip("\n"))
+
+    async with aiofiles.open("goodbye_channels.txt", mode="r") as file:
+        lines = await file.readlines()
+        for line in lines:
+            data = line.split(" ")
+            client.goodbye_channels[int(data[0])] = (int(data[1]), " ".join(data[2:]).strip("\n"))
+
+    async with aiofiles.open("ticket_configs.txt", mode="r") as file:
+        lines = await file.readlines()
+        for line in lines:
+            data = line.split(" ")
+            client.ticket_configs[int(data[0])] = [int(data[1]), int(data[2]), int(data[3])]
+
+    print("Your bot is ready.")
+
+@client.event
+async def on_raw_reaction_add(payload):
+    for role_id, msg_id, emoji in bot.reaction_roles:
+        if msg_id == payload.message_id and emoji == str(payload.emoji.name.encode("utf-8")):
+            await payload.member.add_roles(client.get_guild(payload.guild_id).get_role(role_id))
+            return
+
+    if payload.member.id != client.user.id and str(payload.emoji) == u"\U0001F3AB":
+        msg_id, channel_id, category_id = client.ticket_configs[payload.guild_id]
+
+        if payload.message_id == msg_id:
+            guild = client.get_guild(payload.guild_id)
+
+            for category in guild.categories:
+                if category.id == category_id:
+                    break
+
+            channel = guild.get_channel(channel_id)
+
+            ticket_num = 1 if len(category.channels) == 0 else int(category.channels[-1].name.split("-")[1]) + 1
+            ticket_channel = await category.create_text_channel(f"ticket {ticket_num}", topic=f"A channel for ticket number {ticket_num}.", permission_synced=True)
+
+            await ticket_channel.set_permissions(payload.member, read_messages=True, send_messages=True)
+
+            message = await channel.fetch_message(msg_id)
+            await message.remove_reaction(payload.emoji, payload.member)
+
+            await ticket_channel.send(f"{payload.member.mention} Thank you for creating a ticket! Use **'-close'** to close your ticket.")
+
+            try:
+                await client.wait_for("message", check=lambda m: m.channel == ticket_channel and m.author == payload.member and m.content == "-close", timeout=3600)
+
+            except asyncio.TimeoutError:
+                await ticket_channel.delete()
+
+            else:
+                await ticket_channel.delete()
+
+@client.event
+async def on_raw_reaction_remove(payload):
+    for role_id, msg_id, emoji in bot.reaction_roles:
+        if msg_id == payload.message_id and emoji == str(payload.emoji.name.encode("utf-8")):
+            guild = client.get_guild(payload.guild_id)
+            await guild.get_member(payload.user_id).remove_roles(guild.get_role(role_id))
+            return
+
+@client.event
+async def on_member_join(member):
+    for guild_id in client.welcome_channels:
+        if guild_id == member.guild.id:
+            channel_id, message = client.welcome_channels[guild_id]
+            await client.get_guild(guild_id).get_channel(channel_id).send(f"{message} {member.mention}")
+            return
+
+@client.event
+async def on_member_remove(member):
+    for guild_id in client.goodbye_channels:
+        if guild_id == member.guild.id:
+            channel_id, message = client.goodbye_channels[guild_id]
+            await client.get_guild(guild_id).get_channel(channel_id).send(f"{message} {member.mention}")
+            return
+
+@client.event
+async def on_message_delete(message):
+    client.sniped_messages[message.guild.id] = (message.content, message.author, message.channel.name, message.created_at)
+
+#bot commands
+@client.command()
+async def configure_ticket(ctx, msg: discord.Message=None, category: discord.CategoryChannel=None):
+    if msg is None or category is None:
+        await ctx.channel.send("Failed to configure the ticket as an argument was not given or was invalid.")
+        return
+
+    client.ticket_configs[ctx.guild.id] = [msg.id, msg.channel.id, category.id] # this resets the configuration
+
+    async with aiofiles.open("ticket_configs.txt", mode="r") as file:
+        data = await file.readlines()
+
+    async with aiofiles.open("ticket_configs.txt", mode="w") as file:
+        await file.write(f"{ctx.guild.id} {msg.id} {msg.channel.id} {category.id}\n")
+
+        for line in data:
+            if int(line.split(" ")[0]) != ctx.guild.id:
+                await file.write(line)
+                
+
+    await msg.add_reaction(u"\U0001F3AB")
+    await ctx.channel.send("Succesfully configured the ticket system.")
+
+@client.command()
+async def ticket_config(ctx):
+    try:
+        msg_id, channel_id, category_id = client.ticket_configs[ctx.guild.id]
+
+    except KeyError:
+        await ctx.channel.send("You have not configured the ticket system yet.")
+
+    else:
+        embed = discord.Embed(title="Ticket System Configurations", color=discord.Color.green())
+        embed.description = f"**Reaction Message ID** : {msg_id}\n"
+        embed.description += f"**Ticket Category ID** : {category_id}\n\n"
+
+        await ctx.channel.send(embed=embed)
+        
+@client.command()
+async def snipe(ctx):
+    try:
+        contents, author, channel_name, time = bot.sniped_messages[ctx.guild.id]
+        
+    except:
+        await ctx.channel.send("Couldn't find a message to snipe!")
+        return
+
+    embed = discord.Embed(description=contents, color=discord.Color.purple(), timestamp=time)
+    embed.set_author(name=f"{author.name}#{author.discriminator}", icon_url=author.avatar_url)
+    embed.set_footer(text=f"Deleted in : #{channel_name}")
+
+    await ctx.channel.send(embed=embed)
+    
+@client.command()
+async def set_reaction(ctx, role: discord.Role=None, msg: discord.Message=None, emoji=None):
+    if role != None and msg != None and emoji != None:
+        await msg.add_reaction(emoji)
+        client.reaction_roles.append((role.id, msg.id, str(emoji.encode("utf-8"))))
+        
+        async with aiofiles.open("reaction_roles.txt", mode="a") as file:
+            emoji_utf = emoji.encode("utf-8")
+            await file.write(f"{role.id} {msg.id} {emoji_utf}\n")
+
+        await ctx.channel.send("Reaction has been set.")
+        
+    else:
+        await ctx.send("Invalid arguments.")
+
+@client.command()
+async def set_welcome_channel(ctx, new_channel: discord.TextChannel=None, *, message=None):
+    if new_channel != None and message != None:
+        for channel in ctx.guild.channels:
+            if channel == new_channel:
+                client.welcome_channels[ctx.guild.id] = (channel.id, message)
+                await ctx.channel.send(f"Welcome channel has been set to: {channel.name} with the message {message}")
+                await channel.send("This is the new welcome channel!")
+                
+                async with aiofiles.open("welcome_channels.txt", mode="a") as file:
+                    await file.write(f"{ctx.guild.id} {new_channel.id} {message}\n")
+
+                return
+
+        await ctx.channel.send("Couldn't find the given channel.")
+
+    else:
+        await ctx.channel.send("You didn't include the name of a welcome channel or a welcome message.")
+
+@client.command()
+async def set_goodbye_channel(ctx, new_channel: discord.TextChannel=None, *, message=None):
+    if new_channel != None and message != None:
+        for channel in ctx.guild.channels:
+            if channel == new_channel:
+                client.goodbye_channels[ctx.guild.id] = (channel.id, message)
+                await ctx.channel.send(f"Goodbye channel has been set to: {channel.name} with the message {message}")
+                await channel.send("This is the new goodbye channel!")
+                
+                async with aiofiles.open("goodbye_channels.txt", mode="a") as file:
+                    await file.write(f"{ctx.guild.id} {new_channel.id} {message}\n")
+
+                return
+
+        await ctx.channel.send("Couldn't find the given channel.")
+
+    else:
+        await ctx.channel.send("You didn't include the name of a goodbye channel or a goodbye message.")
+
+@client.command()
+async def hello(ctx):
+    channel = ctx.channel
+    await channel.send("Hi! " + str(ctx.author.mention))
+
+@client.command()
+async def ping(ctx, arg=None, option=1):
+    if arg == "pong":
+        await ctx.channel.send("You've already ponged yourself!")
+
+    else:
+        await ctx.channel.send(str(ctx.author.mention) + " Pong!")
+
+    if option == 1:
+        await ctx.channel.send("You chose option one!")
+
+    else:
+        await ctx.channel.send("You chose option " + str(option))
+
+@client.command()
+async def repeat(ctx, *, arg=None):
+    if arg == None:
+        await ctx.channel.send("You forgot to include an argument.")
+    else:
+        await ctx.channel.send(str(ctx.author.mention) + " " + str(arg))
+
+@client.command()
+async def dm(ctx, user_id=None, *, args=None):
+    if user_id != None and args != None:
+        try:
+            target = await bot.fetch_user(user_id)
+            await target.send(args)
+
+            await ctx.channel.send("'" + args + "' sent to: " + target.name)
+
+        except:
+            await ctx.channel.send("Couldn't dm the given user.")
+        
+
+    else:
+        await ctx.channel.send("You didn't provide a user's id and/or a message.")
+
+@client.command()
+async def dm_all(ctx, *, args=None):
+    if args != None:
+        members = ctx.guild.members
+        for member in members:
+            try:
+                await member.send(args)
+                print("'" + args + "' sent to: " + member.name)
+
+            except:
+                print("Couldn't send '" + args + "' to: " + member.name)
+
+    else:
+        await ctx.channel.send("A message was not provided.")
+
+client.run("token")

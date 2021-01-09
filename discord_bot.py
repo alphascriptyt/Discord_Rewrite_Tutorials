95f3301a6e4faca379c0eb242d168360f1f4ca6a
alphascriptyt <oliver.pickford@outlook.com>
2021-01-09 22:39:31 +0000

    update for warning system

--- discord_bot.py
+++ discord_bot.py
@@ -13,6 +13,13 @@ async def on_ready():
     bot.goodbye_channels = {}
     bot.sniped_messages = {}
     bot.ticket_configs = {}
+    bot.warnings = {}
+    
+    for guild in bot.guilds:
+        async with aiofiles.open(f"{guild.id}.txt", mode="a") as temp:
+            pass
+
+        bot.warnings[guild.id] = {}
     
     for file in ["reaction_roles.txt", "welcome_channels.txt", "goodbye_channels.txt", "ticket_configs.txt"]:
         async with aiofiles.open(file, mode="a") as temp:
@@ -42,7 +49,28 @@ async def on_ready():
             data = line.split(" ")
             bot.ticket_configs[int(data[0])] = [int(data[1]), int(data[2]), int(data[3])]
 
+    for guild in bot.guilds:
+        async with aiofiles.open(f"{guild.id}.txt", mode="r") as file:
+            lines = await file.readlines()
+
+            for line in lines:
+                data = line.split(" ")
+                member_id = int(data[0])
+                admin_id = int(data[1])
+                reason = " ".join(data[2:]).strip("\n")
+
+                try:
+                    bot.warnings[guild.id][member_id][0] += 1
+                    bot.warnings[guild.id][member_id][1].append((admin_id, reason))
+
+                except KeyError:
+                    bot.warnings[guild.id][member_id] = [1, [(admin_id, reason)]]
+                    
     print("Your bot is ready.")
+
+@bot.event
+async def on_guild_join(guild):
+    bot.warnings[guild.id] = {}
 
 @bot.event
 async def on_raw_reaction_add(payload):
@@ -111,6 +139,50 @@ async def on_message_delete(message):
     bot.sniped_messages[message.guild.id] = (message.content, message.author, message.channel.name, message.created_at)
 
 #bot commands
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def warnings(ctx, member: discord.Member=None):
+    if member is None:
+        return await ctx.send("The provided member could not be found or you forgot to provide one.")
+    
+    embed = discord.Embed(title=f"Displaying Warnings for {member.name}", description="", colour=discord.Colour.red())
+    try:
+        i = 1
+        for admin_id, reason in bot.warnings[ctx.guild.id][member.id][1]:
+            admin = ctx.guild.get_member(admin_id)
+            embed.description += f"**Warning {i}** given by: {admin.mention} for: *'{reason}'*.\n"
+            i += 1
+
+        await ctx.send(embed=embed)
+
+    except KeyError: # no warnings
+        await ctx.send("This user has no warnings.")
+        
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def warn(ctx, member: discord.Member=None, *, reason=None):
+    if member is None:
+        return await ctx.send("The provided member could not be found or you forgot to provide one.")
+        
+    if reason is None:
+        return await ctx.send("Please provide a reason for warning this user.")
+
+    try:
+        first_warning = False
+        bot.warnings[ctx.guild.id][member.id][0] += 1
+        bot.warnings[ctx.guild.id][member.id][1].append((ctx.author.id, reason))
+
+    except KeyError:
+        first_warning = True
+        bot.warnings[ctx.guild.id][member.id] = [1, [(ctx.author.id, reason)]]
+
+    count = bot.warnings[ctx.guild.id][member.id][0]
+
+    async with aiofiles.open(f"{member.guild.id}.txt", mode="a") as file:
+        await file.write(f"{member.id} {ctx.author.id} {reason}\n")
+
+    await ctx.send(f"{member.mention} has {count} {'warning' if first_warning else 'warnings'}.")
+
 @bot.command()
 async def configure_ticket(ctx, msg: discord.Message=None, category: discord.CategoryChannel=None):
     if msg is None or category is None:

117c14962bb1abbcbd47a1d0112fd4faa71ed6ac
alphascriptyt <oliver.pickford@outlook.com>
2021-08-08 10:54:34 +0100

    add resume/pause commands to ep16 music

--- episodes/episode-16.py
+++ episodes/episode-16.py
@@ -183,6 +183,26 @@ class Player(commands.Cog):
             ctx.voice_client.stop()
             await self.check_queue(ctx)
 
+
+    @commands.commmand()
+    async def pause(self, ctx):
+        if ctx.voice_client.is_paused():
+            return await ctx.send("I am already paused.")
+
+        ctx.voice_client.pause()
+        await ctx.send("The current song has been paused.")
+
+    @commands.command()
+    async def resume(self, ctx):
+        if ctx.voice_client is None:
+            return await ctx.send("I am not connected to a voice channel.")
+
+        if not ctx.voice_client.is_paused():
+            return await ctx.send("I am already playing a song.")
+        
+        ctx.voice_client.resume()
+        await ctx.send("The current song has been resumed.")`
+
 async def setup():
     await bot.wait_until_ready()
     bot.add_cog(Player(bot))
